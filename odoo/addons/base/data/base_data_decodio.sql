d5da4ee61846ec72aa92bfbb1fbd093221aa34f7
goran.kliska <goran.kliska@decod.io>
2020-04-28 12:04:30 +0200

    [DECODIO] Clean database init script

--- odoo/addons/base/data/base_data_decodio.sql
+++ odoo/addons/base/data/base_data_decodio.sql
@@ -1,115 +1,13 @@
 -------------------------------------------------------------------------
 -- Pure SQL
 -------------------------------------------------------------------------
-
--------------------------------------------------------------------------
--- Postgres tools
--------------------------------------------------------------------------
--- expected from template database
-CREATE EXTENSION IF NOT EXISTS "pg_trgm";
-CREATE EXTENSION IF NOT EXISTS "unaccent";
-
---CREATE EXTENSION IF NOT EXISTS "postgis";
---CREATE EXTENSION IF NOT EXISTS "postgis_topology";
-
-CREATE EXTENSION IF NOT EXISTS "tablefunc";
-CREATE EXTENSION IF NOT EXISTS "adminpack";
-CREATE EXTENSION IF NOT EXISTS "postgres_fdw";
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "ltree";
-CREATE EXTENSION IF NOT EXISTS "hstore";
-CREATE EXTENSION IF NOT EXISTS plpython3u;
-
--------------------------------------------------------------------------
--- Database wide IR dictionary
--------------------------------------------------------------------------
-CREATE TABLE ir_serial (
-    id bigint NOT NULL,
-    name varchar,
-    guid uuid DEFAULT uuid_generate_v4(),
-    schema_name varchar,
-    table_name varchar,
-    ir_model_id bigint, -- references ir_model without FK!
-
-    create_date timestamp without time zone,
-    write_date  timestamp without time zone,
-    delete_date timestamp without time zone,
-    create_uid bigint, --references res_users on delete ? restrict or set null,
-    write_uid bigint,
-    delete_uid bigint,
-    CONSTRAINT ir_serial_pkey PRIMARY KEY (id)
-);
-CREATE SEQUENCE ir_serial_id_seq;
-
--- BOLE: Will not need this - NO HARDCODING ids!!!
---SELECT setval('ir_serial_id_seq', 1000); -- to allow initial inserts
-
-CREATE TABLE ir_audit (
-  id bigserial NOT NULL,
-  ir_serial_id bigint, -- references ir_serial
-  change_date timestamp without time zone DEFAULT (now() AT TIME ZONE 'UTC'),
-  operation varchar,
-  values_before hstore,
-  values_after  hstore,
-  CONSTRAINT ir_audit_pkey PRIMARY KEY (id)
- );
-
-CREATE OR REPLACE FUNCTION oe_audit()
-  RETURNS trigger
-  LANGUAGE plpgsql
-AS $$
-DECLARE
-   _uid bigint := 1; -- blame the admin :(
-   _values_before hstore;
-   _excluded_cols text[] := '{"id","parent_left","parent_right","level"}' ;
-BEGIN
-    IF TG_OP = 'INSERT' THEN
-        IF hstore(NEW) ? 'create_uid' THEN
-           _uid = coalesce(NEW.create_uid, 1);
-        END IF;
-        -- insert all rows from all tables using ir_serial_id_seq for id
-        INSERT INTO ir_serial ( id, table_name,     schema_name, create_uid, create_date)
-                    VALUES (NEW.id, TG_RELNAME, TG_TABLE_SCHEMA,       _uid, now() AT TIME ZONE 'UTC');
-    ELSE
-        IF hstore(OLD) ? 'write_uid' THEN
-           _uid = coalesce(OLD.write_uid, 1); --TODO this is wrong for now...
-        END IF;
-        IF TG_OP = 'DELETE' THEN
-            UPDATE ir_serial SET (delete_uid, delete_date             )
-                                =(_uid      , now() AT TIME ZONE 'UTC')
-             WHERE id = OLD.id;
-            IF TG_ARGV[0] IS NOT NULL THEN  -- 1. parameter log values after updates and deletes?
-               INSERT INTO ir_audit (ir_serial_id, operation, values_before)
-                             VALUES (      OLD.id,     TG_OP, hstore(OLD)  );  -- row_to_json(OLD)
-            END IF;
-        END IF;
-        IF TG_OP = 'UPDATE' THEN
-            UPDATE ir_serial SET (write_uid, write_date              )
-                                =(_uid     , now() AT TIME ZONE 'UTC')
-             WHERE id = OLD.id;
-            IF TG_ARGV[0] IS NULL THEN  RETURN NULL; END IF;-- 1. parameter log values after updates and deletes?
-            IF TG_ARGV[1] IS NOT NULL THEN -- -- 2. parameter holds array of excluded cols for this table
-               _excluded_cols := _excluded_cols || TG_ARGV[1]::text[];
-            END IF;
-             _values_before =  (hstore(NEW.*) - hstore(OLD.*)) - _excluded_cols;
-            IF _values_before = hstore('') THEN RETURN NULL; END IF; -- All changed fields are ignored. Skip this update.
-            INSERT INTO ir_audit (ir_serial_id, operation, values_before)
-                          VALUES (      OLD.id,     TG_OP, _values_before);  -- row_to_json(OLD)
-        END IF;
-    END IF;
-    RETURN NULL;
-END;
-$$;
-
--------------------------------------------------------------------------
 -- DECODIO CHANGES:
--- "id serial," -> "id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),"
--- "id serial NOT NULL,"  -> "id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),"
+-- "id serial," -> "id BIGSERIAL"
 -- "integer" -> "bigint"
 -------------------------------------------------------------------------
 
 CREATE TABLE ir_actions (
-  id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+  id BIGSERIAL,
   primary key(id)
 );
 CREATE TABLE ir_act_window (primary key(id)) INHERITS (ir_actions);
@@ -119,7 +17,7 @@ CREATE TABLE ir_act_server (primary key(id)) INHERITS (ir_actions);
 CREATE TABLE ir_act_client (primary key(id)) INHERITS (ir_actions);
 
 CREATE TABLE res_users (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     active boolean default True,
     login varchar(64) NOT NULL UNIQUE,
     password varchar default null,
@@ -132,13 +30,13 @@ CREATE TABLE res_users (
 );
 
 CREATE TABLE res_groups (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     name varchar NOT NULL,
     primary key(id)
 );
 
 CREATE TABLE ir_module_category (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     create_uid bigint, -- references res_users on delete set null,
     create_date timestamp without time zone,
     write_date timestamp without time zone,
@@ -149,7 +47,7 @@ CREATE TABLE ir_module_category (
 );
 
 CREATE TABLE ir_module_module (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     create_uid bigint, -- references res_users on delete set null,
     create_date timestamp without time zone,
     write_date timestamp without time zone,
@@ -176,7 +74,7 @@ CREATE TABLE ir_module_module (
 ALTER TABLE ir_module_module add constraint name_uniq unique (name);
 
 CREATE TABLE ir_module_module_dependency (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     create_uid bigint, -- references res_users on delete set null,
     create_date timestamp without time zone,
     write_date timestamp without time zone,
@@ -187,7 +85,7 @@ CREATE TABLE ir_module_module_dependency (
 );
 
 CREATE TABLE ir_model_data (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     create_uid bigint,
     create_date timestamp without time zone,
     write_date timestamp without time zone,
@@ -203,14 +101,14 @@ CREATE TABLE ir_model_data (
 );
 
 CREATE TABLE res_currency (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     name varchar NOT NULL,
     symbol varchar NOT NULL,
     primary key(id)
 );
 
 CREATE TABLE res_company (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     name varchar NOT NULL,
     partner_id bigint,
     currency_id bigint,
@@ -220,7 +118,7 @@ CREATE TABLE res_company (
 );
 
 CREATE TABLE res_partner (
-    id bigint NOT NULL DEFAULT nextval('ir_serial_id_seq'),
+    id BIGSERIAL,
     name varchar,
     company_id bigint,
     create_date timestamp without time zone,
@@ -231,98 +129,25 @@ CREATE TABLE res_partner (
 ---------------------------------
 -- Default data
 ---------------------------------
+insert into res_currency (id, name, symbol) VALUES (1, 'EUR', '€');
+insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('EUR', 'base', 'res.currency', true, 1);
+select setval('res_currency_id_seq', 1);
 
--- -> this be default odoo style
---insert into res_currency (id, name, symbol) VALUES (1, 'EUR', '€');
---insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('EUR', 'base', 'res.currency', true, 1);
---select setval('res_currency_id_seq', 1);
+insert into res_company (id, name, partner_id, currency_id, create_date) VALUES (1, 'My Company', 1, 1, now() at time zone 'UTC');
+insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('main_company', 'base', 'res.company', true, 1);
+select setval('res_company_id_seq', 1);
 
--- -> this be decodio modified style
---INSERT INTO res_currency (id, name, symbol)
---	VALUES (DEFAULT, 'EUR', '€')
---	RETURNING id INTO new_currency_id;
---INSERT INTO ir_model_data (name, module, model, noupdate, res_id)
---VALUES ('EUR', 'base', 'res.currency', true, ':new_currency_id')
---;
---
----- company
---SET new_company_id (
---	INSERT INTO res_company (id, name, currency_id, create_date)
---	VALUES (DEFAULT, 'My Company', new_currency_id, now() at time zone 'UTC')
---	RETURNING id
---    );
---INSERT INTO ir_model_data (name, module, model, noupdate, res_id )
---VALUES ('main_company', 'base', 'res.company', true, new_company_id);
---
-----partner
---SET new_partner_id AS (
---    INSERT INTO res_partner (id, name, company_id, create_date)
---    VALUES (DEFAULT, 'My Company', new_company_id, now() at time zone 'UTC')
---    RETURNING id
---    );
---INSERT INTO ir_model_data (name, module, model, noupdate, res_id )
---VALUES ('main_partner', 'base', 'res.partner', true, new_partner_id);
---
---UPDATE res_company set partner_id=new_partner_id where id=new_company_id;
+insert into res_partner (id, name, company_id, create_date) VALUES (1, 'My Company', 1, now() at time zone 'UTC');
+insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('main_partner', 'base', 'res.partner', true, 1);
+select setval('res_partner_id_seq', 1);
 
---users
---WITH users AS (
---    INSERT INTO res_users (id, login, password, active, partner_id,
---                            company_id, create_date) VALUES (
---                            DEFAULT, '__system__', NULL, false,
---                            new_partner_id, new_company_id,
---                            now() at time zone 'UTC')
---    RETURNING id as new_user_id
---    )
---INSERT INTO ir_model_data (name, module, model, noupdate, res_id )
---SELECT 'user_root', 'base', 'res.users', true, new_user_id
---FROM users;
---
----- groups
---WITH user_groups AS (
---    INSERT INTO res_groups (id, name) VALUES (DEFAULT, 'Employee')
---    RETURNING id as new_group_id
---    )
---INSERT INTO ir_model_data (name, module, model, noupdate, res_id )
---SELECT 'group_user', 'base', 'res.groups', true, new_group_id
---FROM user_groups;
---
----------------------------------
--- Default data
----------------------------------
-insert into res_currency (id, name, symbol) VALUES (10, 'EUR', '€');
-insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('EUR', 'base', 'res.currency', true, 10);
---select setval('res_currency_id_seq', 2);
-
-insert into res_company (id, name, partner_id, currency_id) VALUES (20, 'My Company', 30, 10);
-insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('main_company', 'base', 'res.company', true, 20);
---select setval('res_company_id_seq', 2);
-
-insert into res_partner (id, name, company_id) VALUES (30, 'My Company', 20);
-insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('main_partner', 'base', 'res.partner', true, 30);
---select setval('res_partner_id_seq', 2);
-
--- BOLE: imamo 2 admin usera... check: odoo/addons/base/data/res_users_data.xml !!!
-insert into res_users (id, login, password, active, partner_id, company_id) VALUES (1, 'root', 'root', true, 30, 20);
+insert into res_users (id, login, password, active, partner_id, company_id, create_date) VALUES (1, '__system__', NULL, false, 1, 1, now() at time zone 'UTC');
 insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('user_root', 'base', 'res.users', true, 1);
---select setval('res_users_id_seq', 2);
+select setval('res_users_id_seq', 1);
 
-insert into res_groups (id, name) VALUES (40, 'Employee');
-insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('group_user', 'base', 'res.groups', true, 40);
---select setval('res_groups_id_seq', 2);
-
-
--- Handle "ERROR: relation "xy" does not exist" overriding nextval()
-create or replace function nextval(_seq_name text) returns bigint
-as
-$$
-begin
-  return nextval(_seq_name::regclass);
-exception
-  when undefined_table then
-    return nextval('ir_serial_id_seq');
-end;
-$$ language 'plpgsql';
+insert into res_groups (id, name) VALUES (1, 'Employee');
+insert into ir_model_data (name, module, model, noupdate, res_id) VALUES ('group_user', 'base', 'res.groups', true, 1);
+select setval('res_groups_id_seq', 1);
 
 
 create or replace function round(numeric, bigint) returns numeric
@@ -331,4 +156,3 @@ begin
     return(select round($1::numeric,$2::int));
 end;
 $$ language 'plpgsql';
-
